61bf25f933e37f295cc53055932c06445c398cb6
guidonet082020 <hippocampus93@gmail.com>
2020-04-19 11:01:25 -0300

    Se reeemplazo if-else por switch

--- Calculadora.java
+++ Calculadora.java
@@ -14,22 +14,23 @@ public class Calculadora {
 			System.out.println("Ingrese su operacion /n 1 para sumar /n 2 para restar /n 3 para multiplicar / n 4 para dividir");
 			opcion = teclado.nextInt(); 
 			
-		} while (opcion <1 && opcion > 4);
-			
-			System.out.println("Ingrese el primer numero");
-			a=teclado.nextInt(); 
-			System.out.println("Ingrese el segundo numero");
-			b=teclado.nextInt(); 
-			
-			if(opcion.equals(1)) {
-				resultado=a+b;
-			}else if (opcion.equals(2)){
+		} switch (opcion) {
+				case 1 :
+					resultado=a+b;
+					break;
+				case 2 :
 					resultado=a-b;
-				} else if (opcion.equals(3)){
-					resultado=a-b;
-				} else {
+					break;
+				case 3 :
+					resultado=a*b;
+					break;
+				case 4 :
 					resultado=a/b;
-				}
+					break;
+				default: System.out.println("Opción no valida");	
+					
+					
+			}
 			System.out.println("El resultado es: " +resultado.toString());
 
 }
